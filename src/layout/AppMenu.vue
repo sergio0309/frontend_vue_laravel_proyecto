ed0814d28af72d038736e3e115b1176a4a532fa3
sergio0309 <sergio.rodriguezaramayo@gmail.com>
2026-02-18 22:52:21 -0400

    vistas

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -19,8 +19,7 @@ const model = ref([
         ]
     },
     {
-        label: 'Gestion Usuarios',
-        path: '/uikit',
+        label: 'Gestion Seguridad',
         items: [
             {
                 label: 'Usuarios',
@@ -28,239 +27,99 @@ const model = ref([
                 to: '/admin/usuario'
             },
             {
-                label: 'Input',
+                label: 'Roles',
                 icon: 'pi pi-fw pi-check-square',
                 to: '/uikit/input'
-            },
-            {
-                label: 'Button',
-                icon: 'pi pi-fw pi-mobile',
-                to: '/uikit/button',
-                class: 'rotated-icon'
-            },
-            {
-                label: 'Table',
-                icon: 'pi pi-fw pi-table',
-                to: '/uikit/table'
-            },
-            {
-                label: 'List',
-                icon: 'pi pi-fw pi-list',
-                to: '/uikit/list'
-            },
-            {
-                label: 'Tree',
-                icon: 'pi pi-fw pi-share-alt',
-                to: '/uikit/tree'
-            },
-            {
-                label: 'Panel',
-                icon: 'pi pi-fw pi-tablet',
-                to: '/uikit/panel'
-            },
-            {
-                label: 'Overlay',
-                icon: 'pi pi-fw pi-clone',
-                to: '/uikit/overlay'
-            },
-            {
-                label: 'Media',
-                icon: 'pi pi-fw pi-image',
-                to: '/uikit/media'
-            },
-            {
-                label: 'Menu',
-                icon: 'pi pi-fw pi-bars',
-                to: '/uikit/menu'
-            },
-            {
-                label: 'Message',
-                icon: 'pi pi-fw pi-comment',
-                to: '/uikit/message'
-            },
-            {
-                label: 'File',
-                icon: 'pi pi-fw pi-file',
-                to: '/uikit/file'
-            },
-            {
-                label: 'Chart',
-                icon: 'pi pi-fw pi-chart-bar',
-                to: '/uikit/charts'
-            },
-            {
-                label: 'Timeline',
-                icon: 'pi pi-fw pi-calendar',
-                to: '/uikit/timeline'
-            },
-            {
-                label: 'Misc',
-                icon: 'pi pi-fw pi-circle',
-                to: '/uikit/misc'
             }
         ]
     },
     {
-        label: 'Prime Blocks',
+        label: 'Inventarios',
         icon: 'pi pi-fw pi-prime',
-        path: '/blocks',
         items: [
             {
-                label: 'Free Blocks',
+                label: 'Categoria',
                 icon: 'pi pi-fw pi-eye',
-                to: '/blocks/free'
+                to: '/admin/categoria'
             },
             {
-                label: 'All Blocks',
+                label: 'Productos',
                 icon: 'pi pi-fw pi-globe',
-                url: 'https://blocks.primevue.org/',
-                target: '_blank'
+                target: '/admin/productos'
             }
         ]
     },
     {
-        label: 'Pages',
-        icon: 'pi pi-fw pi-briefcase',
-        path: '/pages',
+        label: 'Gestion Pedidos',
+        icon: 'pi pi-fw pi-prime',
         items: [
             {
-                label: 'Landing',
-                icon: 'pi pi-fw pi-globe',
-                to: '/landing'
+                label: 'Pedidos',
+                icon: 'pi pi-fw pi-table',
+                to: '/admin/pedidos'
             },
             {
-                label: 'Auth',
-                icon: 'pi pi-fw pi-user',
-                path: '/auth',
-                items: [
-                    {
-                        label: 'Login',
-                        icon: 'pi pi-fw pi-sign-in',
-                        to: '/auth/login'
-                    },
-                    {
-                        label: 'Error',
-                        icon: 'pi pi-fw pi-times-circle',
-                        to: '/auth/error'
-                    },
-                    {
-                        label: 'Access Denied',
-                        icon: 'pi pi-fw pi-lock',
-                        to: '/auth/access'
-                    }
-                ]
+                label: 'Nuevo Pedido',
+                icon: 'pi pi-fw pi-box',
+                target: '/admin/nuevo Pedido'
             },
             {
-                label: 'Crud',
-                icon: 'pi pi-fw pi-pencil',
-                to: '/pages/crud'
-            },
-            {
-                label: 'Not Found',
-                icon: 'pi pi-fw pi-exclamation-circle',
-                to: '/pages/notfound'
-            },
-            {
-                label: 'Empty',
-                icon: 'pi pi-fw pi-circle-off',
-                to: '/pages/empty'
+                label: 'Clientes',
+                icon: 'pi pi-fw pi-users',
+                target: '/admin/nuevo Pedido'
             }
         ]
     },
-    {
-        label: 'Hierarchy',
-        icon: 'pi pi-fw pi-align-left',
-        path: '/hierarchy',
-        items: [
-            {
-                label: 'Submenu 1',
-                icon: 'pi pi-fw pi-align-left',
-                path: '/submenu_1',
-                items: [
-                    {
-                        label: 'Submenu 1.1',
-                        icon: 'pi pi-fw pi-align-left',
-                        path: '/submenu_1_1',
-                        items: [
-                            {
-                                label: 'Submenu 1.1.1',
-                                icon: 'pi pi-fw pi-align-left'
-                            },
-                            {
-                                label: 'Submenu 1.1.2',
-                                icon: 'pi pi-fw pi-align-left'
-                            },
-                            {
-                                label: 'Submenu 1.1.3',
-                                icon: 'pi pi-fw pi-align-left'
-                            }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 1.2',
-                        icon: 'pi pi-fw pi-align-left',
-                        path: '/submenu_1_2',
-                        items: [
-                            {
-                                label: 'Submenu 1.2.1',
-                                icon: 'pi pi-fw pi-align-left'
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                label: 'Submenu 2',
-                icon: 'pi pi-fw pi-align-left',
-                path: '/submenu_2',
-                items: [
-                    {
-                        label: 'Submenu 2.1',
-                        icon: 'pi pi-fw pi-align-left',
-                        path: '/submenu_2_1',
-                        items: [
-                            {
-                                label: 'Submenu 2.1.1',
-                                icon: 'pi pi-fw pi-align-left'
-                            },
-                            {
-                                label: 'Submenu 2.1.2',
-                                icon: 'pi pi-fw pi-align-left'
-                            }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 2.2',
-                        icon: 'pi pi-fw pi-align-left',
-                        path: '/submenu_2_2',
-                        items: [
-                            {
-                                label: 'Submenu 2.2.1',
-                                icon: 'pi pi-fw pi-align-left'
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        label: 'Get Started',
-        path: '/start',
-        items: [
-            {
-                label: 'Documentation',
-                icon: 'pi pi-fw pi-book',
-                to: '/start/documentation'
-            },
-            {
-                label: 'View Source',
-                icon: 'pi pi-fw pi-github',
-                url: 'https://github.com/primefaces/sakai-vue',
-                target: '_blank'
-            }
-        ]
-    }
+    
+    // {
+    //     label: 'Pages',
+    //     icon: 'pi pi-fw pi-briefcase',
+    //     path: '/pages',
+    //     items: [
+    //         {
+    //             label: 'Landing',
+    //             icon: 'pi pi-fw pi-globe',
+    //             to: '/landing'
+    //         },
+    //         {
+    //             label: 'Auth',
+    //             icon: 'pi pi-fw pi-user',
+    //             path: '/auth',
+    //             items: [
+    //                 {
+    //                     label: 'Login',
+    //                     icon: 'pi pi-fw pi-sign-in',
+    //                     to: '/auth/login'
+    //                 },
+    //                 {
+    //                     label: 'Error',
+    //                     icon: 'pi pi-fw pi-times-circle',
+    //                     to: '/auth/error'
+    //                 },
+    //                 {
+    //                     label: 'Access Denied',
+    //                     icon: 'pi pi-fw pi-lock',
+    //                     to: '/auth/access'
+    //                 }
+    //             ]
+    //         },
+    //         {
+    //             label: 'Crud',
+    //             icon: 'pi pi-fw pi-pencil',
+    //             to: '/pages/crud'
+    //         },
+    //         {
+    //             label: 'Not Found',
+    //             icon: 'pi pi-fw pi-exclamation-circle',
+    //             to: '/pages/notfound'
+    //         },
+    //         {
+    //             label: 'Empty',
+    //             icon: 'pi pi-fw pi-circle-off',
+    //             to: '/pages/empty'
+    //         }
+    //     ]
+    // },
 ]);
 </script>
 
--- src/views/admin/usuario/Usuario.vue
+++ src/views/admin/usuario/Usuario.vue
@@ -1,3 +1,76 @@
 <template>
-    <h1>User</h1>
+  <div class="card relative min-h-[200px]">
+
+    <!-- Spinner -->
+    <div
+      v-if="cargando"
+      class="absolute inset-0 flex items-center justify-center bg-white/70 z-10"
+    >
+      <ProgressSpinner
+        style="width:50px;height:50px"
+        strokeWidth="8"
+        fill="transparent"
+        animationDuration=".5s"
+      />
+    </div>
+
+    <h6 class="mb-2">Tailwindcss</h6>
+
+    <table class="min-w-full table-auto border-collapse">
+      <thead>
+        <tr class="bg-blue-500 text-white">
+          <th class="px-4 py-2 border border-gray-300">ID</th>
+          <th class="px-4 py-2 border border-gray-300">NOMBRE</th>
+          <th class="px-4 py-2 border border-gray-300">CORREO</th>
+          <th class="px-4 py-2 border border-gray-300">CREADO EN</th>
+          <th class="px-4 py-2 border border-gray-300">ACCION</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="user in usuarios" :key="user.id" class="odd:bg-gray-100">
+          <td class="px-4 py-2 border border-gray-300">{{ user.id }}</td>
+          <td class="px-4 py-2 border border-gray-300">{{ user.name }}</td>
+          <td class="px-4 py-2 border border-gray-300">{{ user.email }}</td>
+          <td class="px-4 py-2 border border-gray-300">{{ user.created_at }}</td>
+          <td class="px-4 py-2 border border-gray-300">
+            <button class="bg-yellow-500 px-2 py-2 rounded-full text-white">Editar</button>
+            <button class="bg-red-500 px-2 py-2 rounded-full text-white">
+              <i class="pi pi-times"></i>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <div class="card mt-6">
+    <h5>Prime Vue</h5>
+    <DataTable :value="usuarios" tableStyle="min-width: 50rem">
+      <Column field="id" header="id" />
+      <Column field="name" header="Nombre" />
+      <Column field="email" header="Correo" />
+      <Column field="created_at" header="Creado en" />
+    </DataTable>
+  </div>
 </template>
+
+<script setup>
+
+import ProgressSpinner from 'primevue/progressspinner';
+import { onMounted, ref } from 'vue'; 
+import userService from '../../../services/user.service';
+
+    const cargando = ref(false)
+    const usuarios = ref([])
+
+    onMounted(()=>{
+        funListarUsuarios()
+    })
+    async function funListarUsuarios(){
+        cargando.value = true
+        const { data } = await userService.getUsuarios()
+        usuarios.value = data
+        cargando.value = false
+    }
+</script>
